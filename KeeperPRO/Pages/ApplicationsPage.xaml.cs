a2871bfb6f13b8e1b64c2b7f59f67c890395ac69
SKTDLAR <burtovdanil2005@gmail.com>
2023-10-21 00:10:39 +0300

    Предрелизный коммит!

--- KeeperPRO/Pages/ApplicationsPage.xaml.cs
+++ KeeperPRO/Pages/ApplicationsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KeeperPRO.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,136 @@ namespace KeeperPRO.Pages
         {
             InitializeComponent();
             ApplicationsDG.ItemsSource = App.db.AccessApplication.ToList();
+
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AccessApplication application = ApplicationsDG.SelectedItem as AccessApplication;
+            if (application != null & application.EndTime is null)
+            {
+                application.StartTime = DateTime.Now;
+                App.db.SaveChanges();
+                Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Данные уже записаны или введены некорректно");
+            }
+        }
+        private void Refresh()
+        {
+            IEnumerable<AccessApplication> accessApplications = App.db.AccessApplication.ToList();
+            if (DateSortCb.SelectedIndex != 0)
+            {
+
+
+                if (DateSortCb.SelectedIndex == 1)
+                {
+                    accessApplications = accessApplications.Where(x => x.Date == null || DateTime.Today.Subtract((DateTime)x.Date).Days < 3);
+                }
+                else if (DateSortCb.SelectedIndex == 2)
+                {
+                    accessApplications = accessApplications.Where(x => DateTime.Today.Subtract((DateTime)x.Date).Days < 14);
+
+                }
+                else if (DateSortCb.SelectedIndex == 3)
+                {
+                    accessApplications = accessApplications.Where(x => DateTime.Today.Subtract((DateTime)x.Date).Days < 30);
+
+                }
+            }
+                if (TypeSortCb.SelectedIndex != 0)
+                {
+                    if (TypeSortCb.SelectedIndex == 1)
+                    {
+                        accessApplications = accessApplications.Where(x => x.Type == 1);
+                    }
+                    else if (TypeSortCb.SelectedIndex == 2)
+                    {
+                        accessApplications = accessApplications.Where(x => x.Type == 2);
+                    }
+                }
+                if (DepartmentSortCb.SelectedIndex != 0)
+                {
+                    if (DepartmentSortCb.SelectedIndex == 1)
+                    {
+                        accessApplications = accessApplications.Where(x => x.Department == 1);
+                    }
+                    else if (DepartmentSortCb.SelectedIndex == 2)
+                    {
+                        accessApplications = accessApplications.Where(x => x.Department == 2);
+                    }
+                }
+                if (SearchTb.Text != "")
+                          {
+                            accessApplications = accessApplications.Where(x => x.Passport.Contains(SearchTb.Text));
+                          }
+                
+                ApplicationsDG.ItemsSource = accessApplications;
+            }
+        
+
+        private void DateSortCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void TypeSortCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void DepartmentSortCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+
+        }
+
+        private void SearchTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Refresh();
+
+        }
+
+        private void OutBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            AccessApplication application = ApplicationsDG.SelectedItem as AccessApplication;
+            if (application.StartTime is null)
+            {
+                MessageBox.Show("Чел еще не зашел ты че");
+            }
+            else 
+            { 
+                if (application != null & application.EndTime is null)
+                {
+                   application.EndTime = DateTime.Now;
+                   App.db.SaveChanges();
+                   Refresh();
+                }
+                else
+                {
+                    MessageBox.Show("Данные уже записаны или введены некорректно.");
+                }
+            }
+        }
+
     }
 }
+
+                    //        }
+                    //        if (SearchTb.Text != null)
+                    //        {
+                    //            services = services.Where(x => x.Title.Contains(SearchTb.Text));
+                    //        }
+                    //        ServiceWrapPanel.Children.Clear();
+                    //        foreach (var service in services)
+                    //        {
+                    //            ServiceWrapPanel.Children.Add(
+                    //                new ServiceUserControl(service));
+                    //        }
+                    //        CountOfItems.Text = $"{services.Count()} из {App.db.Service.Count()}";
+                    //    }
+                    //}
+                
